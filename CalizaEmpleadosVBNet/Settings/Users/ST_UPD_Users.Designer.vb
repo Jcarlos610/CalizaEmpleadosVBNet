32d6c0c4f254c8f31c12a9213c7696ee159a50ee
Juan Carlos Sanchez <jcarlos.610@hotmail.com>
2026-05-26 20:19:58 -0600

    cambios

--- CalizaEmpleadosVBNet/Settings/Users/ST_UPD_Users.Designer.vb
+++ CalizaEmpleadosVBNet/Settings/Users/ST_UPD_Users.Designer.vb
@@ -43,11 +43,9 @@ Partial Class ST_UPD_Users
         GroupBox1.Controls.Add(TB_UserName)
         GroupBox1.Controls.Add(LB_User)
         GroupBox1.Controls.Add(BT_UpdateUser)
-        GroupBox1.Location = New Point(17, 344)
-        GroupBox1.Margin = New Padding(4, 5, 4, 5)
+        GroupBox1.Location = New Point(12, 206)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Padding = New Padding(4, 5, 4, 5)
-        GroupBox1.Size = New Size(695, 441)
+        GroupBox1.Size = New Size(747, 272)
         GroupBox1.TabIndex = 11
         GroupBox1.TabStop = False
         GroupBox1.Text = "Datos de usuario"
@@ -55,55 +53,51 @@ Partial Class ST_UPD_Users
         ' DGV_RolesSelection
         ' 
         DGV_RolesSelection.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DGV_RolesSelection.Location = New Point(21, 132)
+        DGV_RolesSelection.Location = New Point(15, 79)
+        DGV_RolesSelection.Margin = New Padding(2, 2, 2, 2)
         DGV_RolesSelection.Name = "DGV_RolesSelection"
         DGV_RolesSelection.RowHeadersWidth = 62
-        DGV_RolesSelection.Size = New Size(372, 275)
+        DGV_RolesSelection.Size = New Size(635, 180)
         DGV_RolesSelection.TabIndex = 14
         ' 
         ' TB_Password
         ' 
-        TB_Password.Location = New Point(223, 82)
-        TB_Password.Margin = New Padding(4, 5, 4, 5)
+        TB_Password.Location = New Point(156, 49)
         TB_Password.Name = "TB_Password"
         TB_Password.PasswordChar = "*"c
-        TB_Password.Size = New Size(170, 31)
+        TB_Password.Size = New Size(120, 23)
         TB_Password.TabIndex = 10
         ' 
         ' LB_Password
         ' 
         LB_Password.AutoSize = True
-        LB_Password.Location = New Point(223, 47)
-        LB_Password.Margin = New Padding(4, 0, 4, 0)
+        LB_Password.Location = New Point(156, 28)
         LB_Password.Name = "LB_Password"
-        LB_Password.Size = New Size(101, 25)
+        LB_Password.Size = New Size(67, 15)
         LB_Password.TabIndex = 9
         LB_Password.Text = "Contraseña"
         ' 
         ' TB_UserName
         ' 
-        TB_UserName.Location = New Point(21, 82)
-        TB_UserName.Margin = New Padding(4, 5, 4, 5)
+        TB_UserName.Location = New Point(15, 49)
         TB_UserName.Name = "TB_UserName"
-        TB_UserName.Size = New Size(180, 31)
+        TB_UserName.Size = New Size(127, 23)
         TB_UserName.TabIndex = 8
         ' 
         ' LB_User
         ' 
         LB_User.AutoSize = True
-        LB_User.Location = New Point(21, 47)
-        LB_User.Margin = New Padding(4, 0, 4, 0)
+        LB_User.Location = New Point(15, 28)
         LB_User.Name = "LB_User"
-        LB_User.Size = New Size(162, 25)
+        LB_User.Size = New Size(106, 15)
         LB_User.TabIndex = 7
         LB_User.Text = "Usuario de sistema"
         ' 
         ' BT_UpdateUser
         ' 
-        BT_UpdateUser.Location = New Point(562, 383)
-        BT_UpdateUser.Margin = New Padding(4, 5, 4, 5)
+        BT_UpdateUser.Location = New Point(666, 236)
         BT_UpdateUser.Name = "BT_UpdateUser"
-        BT_UpdateUser.Size = New Size(107, 38)
+        BT_UpdateUser.Size = New Size(75, 23)
         BT_UpdateUser.TabIndex = 11
         BT_UpdateUser.Text = "Actualizar"
         BT_UpdateUser.UseVisualStyleBackColor = True
@@ -113,23 +107,21 @@ Partial Class ST_UPD_Users
         DGV_Roles.AllowUserToAddRows = False
         DGV_Roles.AllowUserToDeleteRows = False
         DGV_Roles.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DGV_Roles.Location = New Point(17, 77)
-        DGV_Roles.Margin = New Padding(4, 5, 4, 5)
+        DGV_Roles.Location = New Point(12, 46)
         DGV_Roles.Name = "DGV_Roles"
         DGV_Roles.ReadOnly = True
         DGV_Roles.RowHeadersWidth = 62
-        DGV_Roles.Size = New Size(695, 226)
+        DGV_Roles.Size = New Size(747, 136)
         DGV_Roles.TabIndex = 14
         ' 
         ' ST_UPD_Users
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.ButtonHighlight
-        ClientSize = New Size(1774, 1050)
+        ClientSize = New Size(1242, 630)
         Controls.Add(DGV_Roles)
         Controls.Add(GroupBox1)
-        Margin = New Padding(4, 5, 4, 5)
         Name = "ST_UPD_Users"
         Text = "Actualizar datos de usuario"
         WindowState = FormWindowState.Maximized
